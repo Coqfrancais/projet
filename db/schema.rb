0e414599af1b0625c2ce053b61631554c64bc319
ulysse galet <ulysse.galet@rennes-sb.com>
2022-07-16 01:10:41 +0200

    nouvelle colonne adresse ajoutée au model Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_225105) do
+ActiveRecord::Schema.define(version: 2022_07_15_225800) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "stars"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "adress"
   end
 
 end
